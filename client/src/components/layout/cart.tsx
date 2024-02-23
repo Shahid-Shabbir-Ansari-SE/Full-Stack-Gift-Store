91308ad4f1f8e08fee8f63975df667201a325ec5
Shahid-Shabbir-Ansari-SE <shahidshabbirse@gmail.com>
2024-02-23 09:49:13 +0500

    fix - lint errors

--- client/src/components/layout/cart.tsx
+++ client/src/components/layout/cart.tsx
@@ -1,14 +1,12 @@
 /* ------------ Packages Import ------------ */
 'use client'
 import React, { useEffect } from 'react'
-import { useCart } from '@/hooks/fetchUserID'
 
 /* --------------- Interfaces --------------- */
 interface cartProps {}
 
 /* ---------------- Component ---------------- */
 const cart: React.FC<cartProps> = ({}) => {
-  useCart()
   return (
     <div>
       <h1>Cart</h1>
